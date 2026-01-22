fee52202c86c91b2645dc1819065bafdb7a68b6d
anjezzaa <anjeza.murati@gmail.com>
2026-01-22 15:55:16 +0100

    added tokens and providers

--- src/app/tasks/tasks-list/tasks.service.ts
+++ src/app/tasks/tasks-list/tasks.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "../task.model";
+import { LoggingService } from "../../logging.service";
 
-@Injectable({
-    providedIn: 'root'
-})
+// @Injectable({
+//     providedIn: 'root'
+// })
 
 export class TasksService{
     private tasks = signal<Task[]>([]);
+    private loggingService = inject(LoggingService);
 
     allTasks = this.tasks.asReadonly();
 
@@ -18,16 +20,15 @@ export class TasksService{
     };
     
     this.tasks.update((oldTasks) => [...oldTasks, newTask ]);
-    
+    this.loggingService.log('ADDED TASK WITH TITLE '+ taskData.title)
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
     this.tasks.update((oldTasks) => oldTasks.map((task)=> 
         task.id === taskId ? {...task, status: newStatus} : task
-
       )
     );
-  }
+    this.loggingService.log('CHANGE TASK STATUS TO ' + newStatus);
+ }
 }
-
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { InjectionToken } from '@angular/core';
 import { AppComponent } from './app/app.component';
-// import { TasksService } from './app/tasks/tasks-list/tasks.service';
+import { LoggingService } from './app/logging.service';
+import { TasksService } from './app/tasks/tasks-list/tasks.service';
 
-// bootstrapApplication(AppComponent, {
-//     providers: [TasksService]
-// }).catch((err) => console.error(err));
+export const TasksServiceToken = new InjectionToken<TasksService>(
+    'tasks-service-token'
+);
 
-bootstrapApplication(AppComponent)
-    .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [{provide: TasksServiceToken, useClass: TasksService }],
+}).catch((err) => console.error(err)
+);
+
+// bootstrapApplication(AppComponent, {providers: [LoggingService]})
+//     .catch((err) => console.error(err)
+// );
